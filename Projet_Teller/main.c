914678e593f15c77b615791956850fa8040395ea
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2016-11-03 16:59:58 +0100

    Commit Initial

--- Projet_Teller/main.c
+++ Projet_Teller/main.c
@@ -7,9 +7,47 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int sigma_v1();
+int teller();
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int x;
+    printf("nombre");
+    scanf ("%d", &x);
+    return teller(x);
+}
+
+
+int sigma_v1(int x){
+    int i = 1, s = 0;
+    do
+    {
+        if (x % i == 0)
+        {
+            // debug
+            //printf ("%d est un diviseur de %d\n",i,x);
+            s = s + i;
+        }
+        i++;
+    }while (i <= (x/2));
+    printf("%d\n", s);
+    return s;
+}
+
+int teller(int x){
+    int i = 2, s = 0;
+    do
+    {
+        if (x % i == 0)
+        {
+            // debug
+            //printf ("%d est un diviseur de %d\n",i,x);
+            s = s + i;
+        }
+        i++;
+    }while (i <= (x/2));
+    printf("%d\n", s);
+    return s;
 }
